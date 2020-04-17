2983a66dfd9c59173f414de70839d2f762221968
Vincent Zhou <fireflysuccess@gmail.com>
2020-04-17 13:27:37 +0800

    Added ampq cloud test
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

---
+++ config/main.go
@@ -0,0 +1,9 @@
+package config
+
+const (
+	host    = "termite.rmq.cloudamqp.com" // (Load balanced)
+	hostRaw = "termite-01.rmq.cloudamqp.com"
+	passwd  = "QrBHPPxbsd8IuIxKrCnX3-RGoLKaFhYI"
+	// AMQPURL with password
+	AMQPURL = "amqp://drdsfaew:QrBHPPxbsd8IuIxKrCnX3-RGoLKaFhYI@termite.rmq.cloudamqp.com/drdsfaew"
+)
