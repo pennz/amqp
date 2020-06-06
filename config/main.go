8853548d145d16972a6873af1813de2e06f2c8c5
Vincent Zhou <fireflysuccess@gmail.com>
2020-06-06 12:02:19 +0800

    update password"
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- config/main.go
+++ config/main.go
@@ -5,5 +5,6 @@ const (
 	hostRaw = "termite-01.rmq.cloudamqp.com"
 	passwd  = "QrBHPPxbsd8IuIxKrCnX3-RGoLKaFhYI"
 	// AMQPURL with password
-	AMQPURL = "amqp://drdsfaew:QrBHPPxbsd8IuIxKrCnX3-RGoLKaFhYI@termite.rmq.cloudamqp.com/drdsfaew"
+	// AMQPURL = "amqp://drdsfaew:QrBHPPxbsd8IuIxKrCnX3-RGoLKaFhYI@termite.rmq.cloudamqp.com/drdsfaew"
+	AMQPURL = "amqp://kaggle:9b83ca70cf4cda89524d2283a4d675f6@pengyuzhou.com/"
 )
