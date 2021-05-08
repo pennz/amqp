0b22f55952ec78c8fbff5fc543ca81b9a62ec197
Vincent Zhou <fireflysuccess@gmail.com>
2021-05-08 08:44:22 +0800

    Added more comments
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- receive_logs_topic.go
+++ receive_logs_topic.go
@@ -85,16 +85,60 @@ func receiveLogTopic() {
 		false,  // no wait
 		nil,    // args
 	)
+	/*
+
+		// Delivery captures the fields for a previously delivered message resident in
+		// a queue to be delivered by the server to a consumer from Channel.Consume or
+		// Channel.Get.
+		type Delivery struct {
+			Acknowledger Acknowledger // the channel from which this delivery arrived
+
+			Headers Table // Application or header exchange table
+
+			// Properties
+			ContentType     string    // MIME content type
+			ContentEncoding string    // MIME content encoding
+			DeliveryMode    uint8     // queue implementation use - non-persistent (1) or persistent (2)
+			Priority        uint8     // queue implementation use - 0 to 9
+			CorrelationId   string    // application use - correlation identifier
+			ReplyTo         string    // application use - address to reply to (ex: RPC)
+			Expiration      string    // implementation use - message expiration spec
+			MessageId       string    // application use - message identifier
+			Timestamp       time.Time // application use - message timestamp
+			Type            string    // application use - message type name
+			UserId          string    // application use - creating user - should be authenticated user
+			AppId           string    // application use - creating application id
+
+			// Valid only with Channel.Consume
+			ConsumerTag string
+
+			// Valid only with Channel.Get
+			MessageCount uint32
+
+			DeliveryTag uint64
+			Redelivered bool
+			Exchange    string // basic.publish exchange
+			RoutingKey  string // basic.publish routing key
+
+			Body []byte
+		}
+	*/
 	failOnError(err, "Failed to register a consumer")
 
-	forever := make(chan bool)
-
-	go func() {
+	messageLoopHandler := func() {
 		for d := range msgs {
-			log.Printf(" [x] %s", d.Body)
+			log.Printf(" [x] %v,  %s", d, d.Body)
 		}
-	}()
+	}
+	go messageLoopHandler()
 
+	forever := make(chan bool)
 	log.Printf(" [*] Waiting for logs. To exit press CTRL+C")
+
+	/*
+		2021/05/08 08:35:10 Binding queue test-queue to exchange test-logs_topic with routing key a.info
+		2021/05/08 08:35:10  [*] Waiting for logs. To exit press CTRL+C
+		2021/05/08 08:35:40  [x] {0xc0000c25a0 map[]   1 0     0001-01-01 00:00:00 +0000 UTC    ctag-./amqp-1 0 1 false  test-queue [110 111 32 100 97 116 97]},  no data
+	*/
 	<-forever
 }
