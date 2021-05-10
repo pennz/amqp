fc1a7bded2b22ea0848abf8753b2cfd0e7c8ce5a
Vincent Zhou <fireflysuccess@gmail.com>
2021-05-10 13:12:22 +0800

    Added session code from package tutorial
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- main.go
+++ main.go
@@ -25,3 +25,30 @@ func main() {
 		emitLogTopic()
 	}
 }
+
+package main
+
+// This exports a Session object that wraps this library. It
+// automatically reconnects when the connection fails, and
+// blocks all pushes until the connection succeeds. It also
+// confirms every outgoing message, so none are lost.
+// It doesn't automatically ack each message, but leaves that
+// to the parent process, since it is usage-dependent.
+//
+// Try running this in one terminal, and `rabbitmq-server` in another.
+// Stop & restart RabbitMQ to see how the queue reacts.
+func main() {
+	name := "job_queue"
+	addr := "amqp://guest:guest@localhost:5672/"
+	queue := New(name, addr)
+	message := []byte("message")
+	// Attempt to push a message every 2 seconds
+	for {
+		time.Sleep(time.Second * 3)
+		if err := queue.Push(message); err != nil {
+			fmt.Printf("Push failed: %s\n", err)
+		} else {
+			fmt.Println("Push succeeded!")
+		}
+	}
+}
